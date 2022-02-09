89fb37619edace4bd5b9aeae2649b81e65a29334
tanshaobo <18235140339@163.com>
2022-02-09 18:11:09 +0800

    feat(components): 算法页布局设定

--- src/views/Algorithm/AlgorithmList/index.vue
+++ src/views/Algorithm/AlgorithmList/index.vue
@@ -1,5 +1,57 @@
 <template>
-  <div>这是canvas列表</div>
+  <section class="container" ref="box">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-card v-for="(item, index) in dataList" :key="item.id" @click="enterDetail(item)">
+      {{ item.label }}
+    </el-card>
+  </section>
 </template>
-<script setup></script>
-<style lang="stylus"></style>
+<script setup>
+import { onMounted, reactive, toRefs, ref } from 'vue'
+import { useRouter } from 'vue-router'
+const dataList = [
+  { id: 1, label: '冒泡排序' },
+  { id: 2, label: '选择排序' },
+  { id: 3, label: '插入排序' },
+  { id: 4, label: '堆排序' },
+  { id: 5, label: '快速排序' },
+  { id: 6, label: '基数排序' },
+  { id: 7, label: '希尔排序' }
+]
+const state = reactive({})
+
+const router = useRouter()
+
+const enterDetail = (item) => {
+  console.log(item)
+  // router.push(item)
+}
+const box = ref(null)
+const computedLayout = () => {
+  console.dir(box.value)
+  let width = box.value.clientWidth
+  console.log(width)
+}
+onMounted(() => {
+  computedLayout()
+})
+const {} = toRefs(state)
+</script>
+<style lang="stylus">
+.container
+  display flex
+  flex-wrap wrap
+  .el-card
+    width 25%
+    max-width 240px
+    cursor pointer
+    margin-bottom 20px
+    margin-right 20px
+</style>
