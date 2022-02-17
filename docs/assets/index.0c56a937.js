ff325273fe3fa5447c4887f46ac3c79a5a489edd
tanshaobo <18235140339@163.com>
2022-02-17 18:19:26 +0800

    build(other): feat(other): 增加公共配置

---
+++ docs/assets/index.0c56a937.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.2bc66519.js";import{o as r,c}from"./vendor.9360c88e.js";const n={};function o(t,s){return r(),c("div",null,"\u7B97\u6CD5\u8BE6\u60C5\u9875")}var d=e(n,[["render",o]]);export{d as default};
