a70fe84b456272271cf316bfffa6a529beaf9527
tanshaobo <18235140339@163.com>
2022-02-17 14:53:28 +0800

    refactor(utils): 变换二维数组函数转为公共函数

--- src/components/layout/Grid/Hooks/handleSourceData.js
+++ src/components/layout/Grid/Hooks/handleSourceData.js
@@ -2,11 +2,11 @@
  * @Author: tanshaobo
  * @Date: 2022-02-10 18:07:45
  * @LastEditors: tanshaobo
- * @LastEditTime: 2022-02-17 14:26:01
+ * @LastEditTime: 2022-02-17 14:52:04
  * @Description: 处理原始数组 补充至列数倍数
  * @FilePath: \trans-color\src\components\layout\Grid\Hooks\handleSourceData.js
  */
-import getShowList from './getShowList'
+import getShowList from '@/utils/getShowList'
 
 const handleSourceData = (dataList, column, fillData) => {
   let remain = column - (dataList.length % column)
