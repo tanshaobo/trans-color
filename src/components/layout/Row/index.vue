c36c1d3693a993a8f54ad88667ab87ebefa49e8b
tanshaobo <18235140339@163.com>
2022-02-28 15:08:40 +0800

    修正行组件样式

--- src/components/layout/Row/index.vue
+++ src/components/layout/Row/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div v-for="(item, index) in values" :key="item.id">
-    <div v-if="label[index]" class="row">
+    <div
+      v-if="label[index]"
+      class="row"
+      :class="{ column: label[index].length + item.length > 20, ...classObject }"
+    >
       <span class="title">
         {{ label[index] }}
       </span>
@@ -29,17 +33,30 @@ const props = defineProps({
     default: 'between'
   }
 })
+const state = reactive({
+  classObject: {}
+})
 const { label, values, type } = toRefs(props)
+state.classObject = {
+  between: type.value == 'between',
+  left: type.value == 'start'
+}
+const { classObject } = toRefs(state)
 </script>
 
 <style lang="stylus" scoped>
 .row
   line-height 36px
   display flex
-  justify-content space-between
   .title
     font-weight bold
     color rgba(51, 51, 51, .8)
   .content
     color rgba(112, 112, 112, .8)
+  &.column
+    flex-direction column
+  &.between
+    justify-content space-between
+  &.left
+    justify-left flex-start
 </style>
