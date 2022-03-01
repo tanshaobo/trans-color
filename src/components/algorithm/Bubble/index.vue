bdea3db3e784d1c679a2e1e3451c5a59433da6a2
tanshaobo <18235140339@163.com>
2022-03-01 16:57:57 +0800

    refactor(hooks): 拆分设置数组函数

--- src/components/algorithm/Bubble/index.vue
+++ src/components/algorithm/Bubble/index.vue
@@ -4,22 +4,9 @@
 <script setup>
 import { ref, nextTick } from 'vue'
 import Rect from './hooks/Rect'
+import setNums from '../hooks/setNums'
 // 生成数组
-let nums = []
-const setNums = () => {
-  for (let i = 0; i < 4; i++) {
-    // 生成一个 0 - 179的有序数组
-    const arr = [...Array(180).keys()]
-    const res = []
-    while (arr.length) {
-      // 打乱
-      const randomIndex = Math.random() * arr.length - 1
-      res.push(arr.splice(randomIndex, 1)[0])
-    }
-    nums = [...nums, ...res]
-  }
-}
-setNums()
+const nums = setNums()
 
 const canvas = ref(null)
 const creatCtx = (canvas) => {
---
+++ src/components/algorithm/Selection/index.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Author: tanshaobo
+ * @Date: 2022-03-01 16:50:23
+ * @LastEditors: tanshaobo
+ * @LastEditTime: 2022-03-01 16:50:25
+ * @Description: file content
+ * @FilePath: \trans-color\src\components\algorithm\Selection\index.vue
+-->
